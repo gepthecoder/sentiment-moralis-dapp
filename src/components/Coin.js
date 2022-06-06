281715b50d5f42a86d3edfd8c52ee211c9e619fa
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-06-06 15:26:09 +0200

    $0026: [App] [Tokens]: call vote function with params on blockchain 💯

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -6,9 +6,17 @@ import LOGO_BTC from "../images/btc.png"
 import LOGO_ETH from "../images/eth.png"
 import LOGO_MATIC from "../images/poly.png"
 
+import { useWeb3ExecuteFunction, useMoralis } from "react-moralis";
+
+
 function Coin({perc, setPerc, token, setModalToken, setVisible }) {
   const [color, setColor] = useState();
   const [logoSymbol, setLogoSymbol] = useState()
+
+  const contractProcessor = useWeb3ExecuteFunction();
+  const { isAuthenticated} = useMoralis();
+
+
 
   useEffect(() => {
     if (perc < 50) {
@@ -29,6 +37,50 @@ function Coin({perc, setPerc, token, setModalToken, setVisible }) {
       setLogoSymbol(LOGO_MATIC);
     }
   }, []);
+
+  async function vote(upDown){
+
+    let options = {
+      contractAddress: "0x9A81aDc9dDF0c0b398d98A6A89Eb0Db3774A8423",
+      functionName: "vote",
+      abi: [
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "_ticker",
+              "type": "string"
+            },
+            {
+              "internalType": "bool",
+              "name": "_vote",
+              "type": "bool"
+            }
+          ],
+          "name": "vote",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        }
+      ],
+      params: {
+        _ticker: token,
+        _vote: upDown,
+      },
+    }
+
+
+    await contractProcessor.fetch({
+      params: options,
+      onSuccess: () => {
+        console.log("vote successful", '✔');
+      },
+      onError: (error) => {
+        alert(error.data.message)
+      }
+    });
+
+  }
 
   return (
     <>
@@ -56,7 +108,11 @@ function Coin({perc, setPerc, token, setModalToken, setVisible }) {
         <div className="votes">
           <Button 
             onClick={() => {
-              setPerc(perc+1)
+              if(isAuthenticated){
+                vote(true)
+              }else{
+                alert("Authenicate to Vote")
+              }
              }} 
             text="Up" 
             theme="primary" 
@@ -66,7 +122,11 @@ function Coin({perc, setPerc, token, setModalToken, setVisible }) {
           <Button
             color="red"
             onClick={() => {
-              setPerc(perc-1)
+              if(isAuthenticated){
+                vote(false)
+              }else{
+                alert("Authenicate to Vote")
+              }
             }}
             text="Down"
             theme="colored"
